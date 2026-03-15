f881c689c6ea8084fe4dd5a153053a54a61a6a5a
RoiSadeh5 <roisadeh5@gmail.com>
2026-03-15 10:51:20 +0200

    Force upload firebase config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAepZ_ocSGhUn2iIYDzU14Pd0CUhRDuIOE",
+  authDomain: "shift-app-730.firebaseapp.com",
+  projectId: "shift-app-730",
+  storageBucket: "shift-app-730.firebasestorage.app",
+  messagingSenderId: "907717626995",
+  appId: "1:907717626995:web:da7d998fce2dc6c060ee06",
+  measurementId: "G-2M842BC9TV"
+};
